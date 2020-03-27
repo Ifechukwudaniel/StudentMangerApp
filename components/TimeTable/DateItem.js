5b7bd8b312ab59913a54f6ef7414167641a132e0
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-03-27 10:35:27 +0100

    date Items implementation

---
+++ components/TimeTable/DateItem.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Container, Header, Left, Body, Right, Button, Title, Text, Icon } from 'native-base'
+import {View, StatusBar, TouchableOpacity} from 'react-native'
+import EStyleSheet from 'react-native-extended-stylesheet'
+import { LinearGradient } from 'expo-linear-gradient';
+ 
+const DateItem = ({active, date, day}) => {
+    return (
+    <TouchableOpacity style={styles.day}>
+        <LinearGradient style={styles.day} colors={active? ['#EA3939', '#971313'] :['#126FFE', '#0B316C']}>
+           <Text style={styles.dayDate}> {date} </Text>
+           <Text  style={styles.dayMonth}> {day} </Text>
+        </LinearGradient>
+    </TouchableOpacity>
+    );
+}
+ 
+const styles = EStyleSheet.create({
+    day:{
+        width:"80rem",
+        height:"80rem",
+        borderRadius: '10rem',
+        marginRight: '20rem',
+        alignContent: 'center',
+    },
+   
+    dayDate:{
+        fontSize: '25rem', 
+        color:"#fff",
+        fontFamily:"Itim", 
+        marginLeft:'20rem',
+        paddingTop: '10rem',
+    },
+    dayMonth:{
+        fontSize: '20rem', 
+        color:"#fff",
+        fontFamily:"Itim",  
+        marginLeft:'18rem'
+    }
+})
+export default DateItem;
