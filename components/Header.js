c18180f386d3a3120d848177b5a9d09529183999
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-03-27 10:35:41 +0100

    Header component

---
+++ components/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {  Header, Left, Body, Right, Button, Title, Text, Icon } from 'native-base'
+import {View, StatusBar} from 'react-native'
+import EStyleSheet from 'react-native-extended-stylesheet'
+ 
+const HeaderComponent = ({onBackPress, screenName}) => {
+    return (
+        <View style={styles.container}>
+        <Header style={styles.header}>
+              <StatusBar  hidden={true} barStyle='light-content' />
+          <Left>
+            <Button transparent onPress={()=>onBackPress()}>
+              <Icon name='arrow-back' style={styles.icon} />
+              <Text style={styles.backText}>Back</Text>
+            </Button>
+          </Left>
+          <Body>
+            <Title style={styles.title}> {screenName}</Title>
+          </Body>
+          <Right>
+          </Right>
+        </Header>
+        </View>
+    );
+}
+const styles = EStyleSheet.create({
+    container:{
+     flex: 1,
+    },
+    header:{
+        backgroundColor: "#252525" ,
+        borderBottomWidth: "0rem",
+    },
+    title:{
+        fontSize: '23rem', 
+        color:"#fff",
+        fontFamily:"Itim",
+    },
+    backText:{
+        fontSize: '20rem', 
+        color:"#fff",
+        fontFamily:"Itim",
+    },
+    icon:{
+        color:"#fff"
+    }
+   
+})
+export default HeaderComponent;
