b423cbfcd15460ab119ced49f2e5fd997854ff2b
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-03-28 10:21:39 +0100

    Added material screen

--- screens/Home/MaterialScreen.js
+++ screens/Home/MaterialScreen.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {StyleSheet, View, ScrollView, Text} from 'react-native';
 import {  Layout } from '@ui-kitten/components';
+import Material from '../../components/Material'
 
-export default function MaterialScreen() {
+export default function MaterialScreen(props) {
   return (
     <Layout style={styles.container}>
       <View style={styles.container}>
-            <View style={{width:100, height:100, backgroundColor:'red'}}>
-
-      </View>
+         <Material {...props}/>
       </View>
     </Layout>
   );
