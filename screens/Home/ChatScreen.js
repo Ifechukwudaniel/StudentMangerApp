be94e847207f44af3324ee9caa9190c9d78db1c9
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-03-28 10:22:21 +0100

    Added chat screen component

--- screens/Home/ChatScreen.js
+++ screens/Home/ChatScreen.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {StyleSheet, View, ScrollView, Text} from 'react-native';
 import {  Layout } from '@ui-kitten/components';
+import Chat from '../../components/Chat'
 
-export default function ChatScreen() {
+export default function ChatScreen(props) {
   return (
     <Layout style={styles.container}>
       <View style={styles.container}>
-      <View style={{width:100, height:100, backgroundColor:'red'}}>
-
-</View>
+        <Chat {...props}/>
       </View>
     </Layout>
   );
