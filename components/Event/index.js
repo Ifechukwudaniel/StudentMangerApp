a7ae1d0076fca076bfe418344c7cd24eccdc83b6
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-03-28 10:24:37 +0100

    Just bunch of changes

---
+++ components/Event/index.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import {View, StatusBar, TouchableOpacity, ScrollView} from 'react-native'
+import EStyleSheet from 'react-native-extended-stylesheet'
+import { LinearGradient } from 'expo-linear-gradient';
+import Header from '../Header'
+import { Text,  } from 'native-base';
+
+
+
+ 
+const Event = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+             <Header  screenName="Events" onBackPress= {()=>navigation.push("Home")} />
+        </View>
+    );
+}
+const styles = EStyleSheet.create({
+    container:{
+     flex: 1,
+    },
+})
+export default Event;
