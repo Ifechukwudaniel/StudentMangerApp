509da551039deae21b9bc83c72d2e7e7bea65658
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-03-28 10:21:56 +0100

    Aded event screen component

--- screens/Home/EventScreen.js
+++ screens/Home/EventScreen.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {StyleSheet, View, ScrollView, Text} from 'react-native';
 import {  Layout } from '@ui-kitten/components';
+import Event from '../../components/Event';
 
-export default function EventScreen() {
+export default function EventScreen(props) {
   return (
     <Layout style={styles.container}>
       <View style={styles.container}>
-      <View style={{width:100, height:100, backgroundColor:'red'}}>
-
-</View>
+          <Event {...props}/>
       </View>
     </Layout>
   );
