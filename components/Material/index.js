d3020e26b998d632ec98559119d3862097c513dc
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-03-31 21:51:15 +0100

    Started workin on Material items

--- components/Material/index.js
+++ components/Material/index.js
@@ -1,57 +1,33 @@
 import React, {useState} from 'react';
 import {View, StatusBar, TouchableOpacity, ScrollView, Modal, TouchableHighlight} from 'react-native'
+import { Input} from 'native-base'
 import EStyleSheet from 'react-native-extended-stylesheet'
-import { LinearGradient } from 'expo-linear-gradient';
-import Header from '../Header'
-import { Text } from 'native-base';
-import Colors from '../../constants/Colors';
+import MaterialItem from './materialItem';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
-
-
+import Ripple from 'react-native-material-ripple';
  
-const Material = ({navigation}) => {
-    const [showModal , setShowModal] = useState(false)
+const Material = () => {
     return (
         <View style={styles.container}>
+           <StatusBar hidden/>
+          <View style={styles.filterView}>
+            <TouchableOpacity style={styles.icon}>
+                 <MaterialIcon  name="filter-list" size={40} color="#fff"/>
+            </TouchableOpacity>
+          <View style={styles.searchView}>
+              <Input style={styles.textBox} placeholder="Search for ..." placeholderTextColor="#AEAEAE"/>
+              <Ripple style={styles.searchIcon}>
+                 <MaterialIcon   name="search" size={30} color="#fff"/>
+              </Ripple>
+          </View>
+          </View>
             <ScrollView style={styles.scrollView}>
-                 <View>
-                    <TouchableOpacity style= {styles.materialItem} onPress={()=>setShowModal(true)}>
-                            <LinearGradient style={styles.gradient} colors= {['#126FFE', '#0B4FB9']}>
-                                <Text style={styles.courseCode}>Csc 131</Text>
-                                <View style={styles.line}/>
-                                <Text style={styles.courseTitle}>Introduction to math and physics </Text>
-                                <Text style={styles.courseTitle}>First material </Text>
-                                <Text style={styles.courseTitle}>Lectures: mr gay & mrs  bitch</Text>
-                                <Text style={[styles.courseTitle, {fontSize:15}]}>Posted on 1st of may</Text>
-                            </LinearGradient>
-                    </TouchableOpacity>
-                    <TouchableOpacity style= {styles.materialItem}>
-                            <LinearGradient style={styles.gradient}  colors= {['#126FFE', '#0B4FB9']}>
-                                <Text style={styles.courseCode}>Csc 121</Text>
-                                <View style={styles.line}/>
-                                <Text style={styles.courseTitle}>Introduction to Quantum physis </Text>
-                                <Text style={styles.courseTitle}>First material </Text>
-                                <Text style={styles.courseTitle}>Lectures: mr gay & mrs  bitch</Text>
-                                <Text style={[styles.courseTitle, {fontSize:15}]}>Posted on 1st of may</Text>
-                            </LinearGradient>
-                    </TouchableOpacity>
-                    <Modal
-                      animationType='slide'
-                       transparent={true}  
-                      visible={ showModal}
-                    
-                     >
-                    <View style={styles.modal}>
-                        <View style={styles.modalView}>
-                          <TouchableOpacity onPress={()=>setShowModal(false)}>
-                             <LinearGradient style={styles.closeButton} colors={['#E82222', 'rgba(232, 34,34,0.71)']}>
-                               <MaterialIcon name="close" size={45} color={Colors.white} style={styles.icon}/>
-                             </LinearGradient>
-                          </TouchableOpacity>
-                        </View>
-                    </View>
-                     </Modal>
-                  </View>
+               <MaterialItem/>
+               <MaterialItem/>
+               <MaterialItem/>
+               <MaterialItem/>
+               <MaterialItem/>
+               <MaterialItem/>
             </ScrollView>
         </View>
     );
@@ -60,72 +36,47 @@ const styles = EStyleSheet.create({
     container:{
      flex: 1,
     },
+    filterView:{
+      marginTop:'47rem',
+      flexDirection: 'row',
+      alignContent: 'center',
+    },
     scrollView:{
-     marginTop:"30%"
+     marginTop:"16%"
     },
-    materialItem:{
-        width:'90%',
-        height:"190rem",
-        alignSelf: 'center',
-        overflow: 'hidden',
-        borderTopLeftRadius: '20rem',
-        borderTopRightRadius: '20rem',
-        borderBottomLeftRadius: '20rem',
-        borderBottomRightRadius: '20rem',
-        marginTop: '10rem',
+    textBox:{
+      width:"60%",
+      height:'40rem',
+      backgroundColor: "#121212",
+      borderWidth: '1rem',
+      borderColor: "#fff",
+      color:"#fff",
+      fontFamily:"Itim",
+      alignSelf: 'flex-end',
+      borderRadius: '5rem',
+      marginLeft:"70rem",
+      borderRightWidth: '0rem',
     },
-    gradient:{
-        height:"190rem",
-        padding: '10rem',
+    icon:{
+      marginLeft: '30rem',
+      alignSelf: 'center',
+    //   justifySelf: 'center',
     },
-    courseCode:{
-        fontSize: '40rem',
-        fontWeight: 'bold',
-        color:"#fff",
+    searchView:{
+       width:"100%",
+       flexDirection:'row'
     },
-    courseTitle:{
-        fontSize: '20rem',
-        color:"#fff",
-        fontFamily:"Itim",
-        marginLeft: "2rem",
-        lineHeight:"20rem",
-        paddingTop: '4rem',
-    },
-    line:{
-        width:"100%",
-        height:"2%",
-        backgroundColor:"#fff",
-        marginTop:'2rem'
-      },
-      modal:{
-        backgroundColor:'transparent',
-        flex:1,
-        justifyContent:'flex-end'
-      },
-      modalView:{
-        backgroundColor:Colors.black,
-        height:'89%',
-        borderTopLeftRadius: "100rem",
-        borderTopRightRadius: "100rem",
-        shadowColor: "#3F4141",
-        shadowOffset: {
-              width: -2.5,
-              height: -2.5,
-        },
-        shadowOpacity: 1,
-        shadowRadius: 4,
-        elevation: 5,
-      },
-      closeButton:{
-         width:'60rem',
-         height:'60rem',
-         alignSelf: 'center',
-         marginTop: '30rem',
-         borderRadius: '30rem',
-      },
-      icon:{
-        alignSelf:'center',
-        marginTop:'5rem'
-      }
+    searchIcon:{
+        left:"-5rem",
+        width:"50rem",
+        height:"40rem",
+        marginRight:"80rem",
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: '1rem',
+        borderColor: "#fff",
+        borderTopRightRadius: '5rem',
+        borderBottomRightRadius: '5rem',
+    }
 })
 export default Material;
---
+++ components/Material/materialItem.js
@@ -0,0 +1,175 @@
+import React, {useState} from 'react';
+import {View, StatusBar, TouchableOpacity, ScrollView, Modal, TouchableHighlight, Image} from 'react-native'
+import EStyleSheet from 'react-native-extended-stylesheet'
+import { LinearGradient } from 'expo-linear-gradient';
+import { Text } from 'native-base';
+import Colors from '../../constants/Colors';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
+
+
+ 
+const MaterialItem = () => {
+    const [showModal , setShowModal] = useState(false)
+    return (
+        <>
+                    <TouchableOpacity style= {styles.materialItem} onPress={()=>setShowModal(true)}>
+                            <LinearGradient style={styles.gradient} colors= {['#126FFE', '#0B4FB9']}>
+                                <Text style={styles.courseCode}>Csc 131</Text>
+                                <Text style={[styles.courseTitle, styles.courseTitleText]}>Introduction to math and physics </Text>
+                                <Text style={styles.courseTitle}>First material </Text>
+                                <Text style={styles.courseTitle}>Lectures: mr gay & mrs  bitch</Text>
+                                <Text style={[styles.courseTitle, {fontSize:15}]}>Material Type : PDF</Text>
+                                <Text style={[styles.courseTitle, {fontSize:15}]}>Posted on 1st of may</Text>
+                            </LinearGradient>
+                    </TouchableOpacity>
+                    <Modal
+                      animationType='slide'
+                      transparent={true}  
+                      visible={ showModal}
+                     >
+                    <View style={styles.modal}>
+                        <View style={styles.modalView}>
+                          <TouchableOpacity onPress={()=>setShowModal(false)}>
+                             <LinearGradient style={styles.closeButton} colors={['#E82222', 'rgba(232, 34,34,0.71)']}>
+                               <MaterialIcon name="close" size={40} color={Colors.white} style={styles.icon}/>
+                             </LinearGradient>
+                          </TouchableOpacity>
+                           <View style={styles.content}>
+                             <View style={styles.imageWrapper}>
+                                <Image style={styles.image} resizeMode='cover' source ={require('../../assets/images/imageDoc.png')}/>
+                             </View>
+                             <View style={styles.description}>
+                                <Text style={styles.courseCode}>Csc 131</Text>
+                                <Text style={[styles.courseTitle, styles.courseTitleTextModal]}>Introduction to math and physics </Text>
+                                <Text style={[styles.courseTitle, styles.courseTitleText]}>First material </Text>
+                                <Text style={[styles.courseTitle, styles.courseTitleText]}>Lectures: mr gay & mrs  bitch</Text>
+                                <Text style={[styles.courseTitle, styles.courseTitleText]}>Material Type : PDF</Text>
+                                <Text style={[styles.courseTitle, styles.courseTitleText]}>Posted on 1st of may</Text>
+                                <Text style={[styles.courseTitle, styles.courseTitleText]}>150 copies printed and delivered</Text>
+                             </View>
+                             <View style={styles.action}>
+                                <TouchableOpacity style={styles.actionIcon}>
+                                    <MaterialIcon name="file-download" size={30} color={Colors.white} style={styles.icon}/>
+                                </TouchableOpacity>  
+                                <TouchableOpacity style={styles.actionIcon}>
+                                    <MaterialIcon name="remove-red-eye" size={30} color={Colors.white} style={styles.icon}/>  
+                                </TouchableOpacity>  
+                                <TouchableOpacity style={styles.actionIcon}>
+                                     <MaterialIcon name="local-printshop" size={30} color={Colors.white} style={styles.icon}/>    
+                                </TouchableOpacity>  
+                                <TouchableOpacity style={styles.actionIcon}>
+                                     <MaterialIcon name="comment" size={30} color={Colors.white} style={styles.icon}/>
+                                </TouchableOpacity>  
+                             </View>
+                           </View>
+                        </View>
+                    </View>
+                     </Modal>
+            </>
+    );
+}
+const styles = EStyleSheet.create({
+    content:{
+      marginTop:"20rem"
+    },
+    materialItem:{
+        width:'90%',
+        height:"200rem",
+        alignSelf: 'center',
+        overflow: 'hidden',
+        borderTopLeftRadius: '20rem',
+        borderTopRightRadius: '20rem',
+        borderBottomLeftRadius: '20rem',
+        borderBottomRightRadius: '20rem',
+        marginTop: '10rem',
+    },
+    gradient:{
+        height:"200rem",
+        padding: '10rem',
+    },
+    courseCode:{
+        fontSize: '37rem',
+        fontWeight: 'bold',
+        color:"#fff",
+    },
+    courseTitle:{
+        fontSize: '20rem',
+        color:"#fff",
+        fontFamily:"Itim",
+        marginLeft: "2rem",
+        lineHeight:"20rem",
+        paddingTop: '4rem',
+    },
+     courseTitleText:{
+        // fontSize: '25rem',
+        paddingTop:'10rem'
+    },
+    courseTitleTextModal:{
+        fontSize: '30rem',
+        paddingTop:'10rem'
+    },
+      modal:{
+        backgroundColor:'transparent',
+        flex:1,
+        justifyContent:'flex-end'
+      },
+      modalView:{
+        backgroundColor:Colors.black,
+        height:'87%',
+        borderTopLeftRadius: "100rem",
+        borderTopRightRadius: "100rem",
+        shadowColor: "#3F4141",
+        shadowOffset: {
+              width: -2.5,
+              height: -2.5,
+        },
+        shadowOpacity: 1,
+        shadowRadius: 4,
+        elevation: 5,
+      },
+      closeButton:{
+         width:'50rem',
+         height:'50rem',
+         alignSelf: 'center',
+         marginTop: '20rem',
+         borderRadius: '25rem',
+         justifyContent: 'center',
+         alignContent: 'center',
+      },
+      icon:{
+        alignSelf:'center',
+        justifyContent: 'center',
+      },
+      imageWrapper:{
+        width:"340rem",
+        height:"200rem",
+        overflow: 'hidden',
+        borderRadius:'10rem',
+        justifyContent: 'center',
+        alignSelf:"center"
+      },
+      image:{
+          width:"340rem",
+          height:"200rem",
+      },
+      description:{
+          marginLeft:"24rem",
+          marginTop: '30rem',
+      },
+      action:{
+       marginLeft:"24rem",
+       marginTop: '30rem',
+       flexDirection: 'row',
+       marginLeft: '30rem',
+      },
+      actionIcon:{
+          width:"50rem",
+          height:"50rem",
+          backgroundColor:"#CF2154",
+          borderRadius:"40rem",
+          marginRight: '40rem',
+          alignContent:"center",
+          justifyContent: 'center',
+      }
+})
+export default MaterialItem;
