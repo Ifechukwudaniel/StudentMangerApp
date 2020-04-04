0f683d55d7b936c9110c06843724e68141d2183b
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-04-04 02:51:20 +0100

    removed test

--- components/__tests__/Material-test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-
-import Material from '../Material';
-
-it(` Material component renders correctly`, () => {
-    renderer.create( <Material navigation={{}}/>).toJSON();
-});
--- components/__tests__/TimeTable-test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-
-import TimeTable from '../TimeTable';
-
-it(` Time Table renders correctly`, () => {
-   renderer.create( <TimeTable navigation={{}}/>);
-});
