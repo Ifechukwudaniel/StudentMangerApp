1b1d4670226a94e16bfba97aea2fe761e2f99174
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-04-08 00:22:24 +0100

    Started working on charts

--- components/Attendance/Chart/index.js
+++ components/Attendance/Chart/index.js
@@ -1,8 +1,41 @@
 import React, { useState } from "react";
-import { View, StatusBar, TouchableOpacity, ScrollView,Text } from "react-native";
+import { View, StatusBar, TouchableOpacity, ScrollView,Text, Dimensions } from "react-native";
 import EStyleSheet from "react-native-extended-stylesheet";
 import { LinearGradient } from "expo-linear-gradient";
 import Header from "../../Header";
+import {
+  LineChart,
+  PieChart
+} from 'react-native-chart-kit'
+const entireScreenWidth = Dimensions.get('window').width;
+const rem = entireScreenWidth / 380;
+
+const line = {
+      labels: ['January', 'February', 'March'],
+      datasets: [
+        {
+          data: [2, 4,0 ],
+          strokeWidth: 1, 
+        },
+      ],
+    };
+
+    const pieData = [
+      {
+        name: 'Present',
+        attendance: 70,
+        color: '#126AF3',
+        legendFontColor: '#7F7F7F',
+        legendFontSize: 15,
+      },
+      {
+        name: 'Absent',
+        attendance: 30,
+        color: '#EA3939',
+        legendFontColor: '#7F7F7F',
+        legendFontSize: 15,
+      }
+    ];
 
 const Chart = ({ navigation }) => {
   return (
@@ -11,10 +44,30 @@ const Chart = ({ navigation }) => {
         <ScrollView style={styles.scrollView}>
              <Text style={styles.headerAttendance} >Csc 122 Attendance</Text>
              <View style={styles.chartView}>
+              <PieChart
+                data={pieData}
+                 width={320*rem} 
+                height={330*rem}
+                chartConfig={{
+                    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  }}
+                accessor="attendance"
+                backgroundColor="transparent"
 
+              />
              </View>
              <View style={styles.chartView}>
-
+                <LineChart
+                  data={line}
+                  width={320*rem} 
+                  height={330*rem}
+                  chartConfig={{
+                    backgroundColor: "red",
+                    decimalPlaces:0,
+                    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`
+                  }}
+                  bezier
+                  style={styles.graph}/>
             </View>
         </ScrollView>
     </View>
@@ -38,7 +91,7 @@ const styles = EStyleSheet.create({
     height:"330rem",
     alignSelf:"center",
     marginTop:"20rem",
-    shadowColor: "#3F4141",
+    shadowColor: "#000",
       shadowOffset: {
             width: '0rem',
             height: '-4rem',
@@ -46,10 +99,14 @@ const styles = EStyleSheet.create({
       shadowOpacity: 1,
       shadowRadius: '3.84rem',
       elevation: '5rem',
-      backgroundColor: "#252525",
+      backgroundColor: "#000",
   },
   scrollView:{
    marginBottom: '20rem',
+  },
+  graph:{
+    height:"300rem",
+    borderRadius:"30rem",
   }
 
 });
