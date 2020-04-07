48019611aa68eafce4106d414c14925fe9c69045
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-04-07 03:01:43 +0100

    Finished attendance navigation

---
+++ navigation/AttendanceNavigation.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator ,} from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import {Icon} from 'native-base'
+import TabBarIcon from '../components/TabBarIcon';
+import colors from "../constants/Colors"
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons'
+import Colors from '../constants/Colors';
+import CalenderScreen from '../screens/CalenderScreen';
+import GraphScreen from '../screens/GraphScreen';
+
+const config = Platform.select({
+  default: {
+  },
+});
+
+const CalenderStack = createStackNavigator(
+  {
+    Calender: CalenderScreen,
+  },
+  config
+);
+
+CalenderStack.navigationOptions = {
+  tabBarLabel: 'Calender',
+  tabBarIcon: ({ focused }, props) => (
+    <MaterialCommunityIcon size={30} color={focused ? Colors.tintColor :"#fff"} name="calendar-blank"/>
+  ),
+};
+
+CalenderStack.path = '';
+
+const GraphStack = createStackNavigator(
+  {
+    Graph: GraphScreen,
+  },
+  config
+);
+
+GraphStack.navigationOptions = {
+  tabBarLabel: 'Graph',
+  tabBarIcon: ({ focused }, props) => (
+    <MaterialIcon size={30} color={focused ? Colors.tintColor :"#fff"} name="show-chart"/>
+  ),
+};
+
+GraphStack.path = '';
+
+const AttendanceNav = createBottomTabNavigator({
+   Calender:CalenderStack,
+   Graph:GraphStack,
+},
+{
+  tabBarOptions:{
+    showLabel:true,
+    activeTintColor:"#fff",
+    labelStyle:{
+     color:colors.white,
+     fontFamily:"itim",
+     fontSize:16
+    },
+    style:{
+        backgroundColor:Colors.black,
+        // marginTop:10     
+    },
+
+  },
+});
+
+AttendanceNav.path = '';
+
+export default AttendanceNav;
---
+++ screens/CalenderScreen.js
@@ -0,0 +1,31 @@
+import React,{Component} from 'react';
+import {StyleSheet, View, ScrollView, Text} from 'react-native';
+import { MonoText } from '../components/StyledText';
+import {  Layout } from '@ui-kitten/components';
+import Downloads from '../components/Downloads';
+
+
+class CalenderScreen extends Component {
+  render() { 
+    return (
+      <Layout style={styles.container}>
+        <View style={styles.container}>
+            {/* <Downloads {...this.props}/> */}
+        </View>   
+    </Layout>
+    );
+  }
+}
+ 
+export default  CalenderScreen;
+
+CalenderScreen.navigationOptions = {
+  header:null
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
