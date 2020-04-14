f1592e36d9f6de40d62ef2feb45a9585a2194dc7
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-04-14 18:13:50 +0100

    redux worked

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View, AppRegistry, Dimensions } from 'react-native';
 import { ApplicationProvider,IconRegistry } from '@ui-kitten/components';
@@ -11,7 +8,7 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 const entireScreenWidth = Dimensions.get('window').width;
 import AppNavigator from './navigation/AppNavigator';
 import appTheme from './theme.json'
-import store  from './Redux/store'
+import {store} from './Redux/store'
 
 const theme = {...dark, ...appTheme}
 
@@ -24,14 +21,14 @@ EStyleSheet.build({
 export default function App(props) {
     return (
       <>
-        {/* //<Provider store={store}> */}
+       <Provider store={store}>
         <IconRegistry icons={EvaIconsPack}/>
           <ApplicationProvider mapping={mapping} theme={theme}   >
             {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
                 <AppNavigator />
           </ApplicationProvider>
 
-      {/* </Provider> */}
+      </Provider>
       </>
     );
 }
--- Redux/store.js
+++ Redux/store.js
@@ -1,5 +1,8 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {allReducers} from './Reducers/allReducers';
 import thunk from 'redux-thunk';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__||compose
 
-export const store = createStore(allReducers, applyMiddleware(thunk));
+export const store = createStore(allReducers, composeEnhancers(
+  applyMiddleware(thunk)
+  ));
