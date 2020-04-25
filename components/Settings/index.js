888dde2c450dddec4c0474f2465c8c1389ddb1bf
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-04-25 19:36:13 +0100

    Started changing  setting

--- components/Settings/index.js
+++ components/Settings/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView} from 'react-native';
+import { View,  StyleSheet, ScrollView} from 'react-native';
 import UserInfo from './extra/user-info-component';
 import DepartmentSetting from './extra/DepartmentSetting';
 import {Grid,Row} from 'react-native-easy-grid'
@@ -7,34 +7,56 @@ import LevelSetting from './extra/LevelSetting';
 import PhoneSetting from './extra/PhoneSetting';
 import GetHelpSetting from './extra/GetHelpSetting';
 import FeedbackSetting from './extra/GiveUsFeedback';
+import { Container, Header, Content, Button, ListItem,  Icon, Left, Body, Right, Switch } from 'native-base';
+import {Text } from '@ui-kitten/components'
  
  
 class Setting extends Component {
     render() { 
         return (
-           <View style={styles.container}>
-            <ScrollView>
-            <Grid>
-                  <UserInfo/>
-                  <Row>
-                        <DepartmentSetting onPress= {()=>{this.props.navigation.navigate('Department')}}/>
-                   </Row>
-                   <Row>
-                        <LevelSetting onPress= {()=>{this.props.navigation.navigate('level')}}/>
-                   </Row>
-                   <Row>
-                        <PhoneSetting onPress= {()=>{this.props.navigation.navigate('phoneSetting')}}/>
-                   </Row>
-                   <Row>
-                        <GetHelpSetting onPress= {()=>{}}/>
-                   </Row>
-                   <Row>
-                        <FeedbackSetting onPress= {()=>{}}/>
-                   </Row>
-                   
-            </Grid>
-            </ScrollView>
-            </View>
+              <View style = {{backgroundColor:"#252525"}} >
+                      <ListItem  onPress= {()=>{this.props.navigation.navigate('Department')}}   icon>
+                        <Left>
+                          <Button style={{ backgroundColor: "#FF9501" }}>
+                            <Icon active name="business" />
+                          </Button>
+                        </Left>
+                        <Body>
+                          <Text style={{fontFamily:'itim'}}>Department </Text>
+                        </Body>
+                        <Right>
+                          <Text>Computer Science </Text>
+                          <Icon active name="arrow-forward" />
+                        </Right>
+                      </ListItem>
+                      <ListItem onPress= {()=>{this.props.navigation.navigate('level')}} icon>
+                        <Left>
+                          <Button style={{ backgroundColor: "#007AFF" }}>
+                            <Icon active name="wifi" />
+                          </Button>
+                        </Left>
+                        <Body>
+                          <Text>Level</Text>
+                        </Body>
+                        <Right>
+                          <Text>100</Text>
+                          <Icon active name="arrow-forward" />
+                        </Right>
+                      </ListItem>
+                      <ListItem onPress= {()=>{this.props.navigation.navigate('phoneSetting')}} icon>
+                        <Left>
+                          <Button style={{ backgroundColor: "#007AFF" }}>
+                            <Icon active name="bluetooth" />
+                          </Button>
+                        </Left>
+                        <Body>
+                          <Text>Phone number</Text>
+                        </Body>
+                        <Right>
+                          <Icon active name="arrow-forward" />
+                        </Right>
+                      </ListItem>
+                    </View>
         );
     }
 }
