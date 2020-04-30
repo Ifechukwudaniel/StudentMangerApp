b40bf7f1e350a74efe649f3584e7e0e286f69f41
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-04-30 17:16:40 +0100

    made small modification

--- util/index.js
+++ util/index.js
@@ -2,9 +2,16 @@ import moment from 'moment'
 import _ from 'lodash'
 
  const convertTimeTable = (timeTable) =>{
+   var count = 0
       _.find(timeTable, {weekDay:moment().weekday()}).active = true
       const sortedDate =   _.sortBy(timeTable, (data)=> data.weekDay<moment().day())
-      _.map(sortedDate, (day, i)=>day.date = moment().date()+i )
+      _.map(sortedDate, (day, i)=> {
+        if( moment().date()+i > moment().daysInMonth()){
+           day.date = ++count
+        }
+        else   day.date = moment().date()+ i
+      })
+
      return sortedDate
  }
 
