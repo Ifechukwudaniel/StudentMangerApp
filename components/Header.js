92061864d470d40a9f30a89b83935190c9dea7c0
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-06-17 21:05:42 +0100

    finished header

--- components/Header.js
+++ components/Header.js
@@ -1,92 +1,54 @@
 import React from 'react';
-import {  Header, Left, Body, Right, Button, Title, Text, Icon } from 'native-base'
-import {View, StatusBar, Platform} from 'react-native'
+import {  Header, Left, Body, Right, Button, Title, Text, Icon,  } from 'native-base'
+import {Image, StatusBar, Dimensions} from 'react-native'
 import EStyleSheet from 'react-native-extended-stylesheet'
- 
+import HandBugger from '../assets/svg/handBugger.svg' 
+const entireScreenWidth = Dimensions.get('window').width;
+const rem = entireScreenWidth/380
+
 const HeaderComponent = ({onBackPress, screenName, style, noBackButton, closeButton, headerStyle}) => {
     return (
-        <View>
-        <Header  style={[styles.header, headerStyle]}>
-              <StatusBar  barStyle='light-content' />
-          <Left>
-            {
-                noBackButton?(
-                    <View/>  
-                ):(
-                    <>
-                    <Button transparent onPress={()=>onBackPress()}>
-                        <Icon name='arrow-back' style={styles.icon} />
-                        { Platform.OS!='android'? <Text style={styles.backText}>Back</Text> :<Text style={styles.backText}></Text>} 
-                     </Button>
-                </>
-                )
-               
-            }
-            {
-                !closeButton?(
-                    <View/>  
-                ):(
-                    <>
-                    <Button transparent onPress={()=>onBackPress()}>
-                        { Platform.OS!='android'? <Text style={styles.backText}>Close </Text> :
-                         (
-                              <>
-                            <Icon name='arrow-back' style={styles.icon} />
-                             <Text style={styles.backText}></Text>
-                             </>
-                        )} 
-                     </Button>
-                </>
-                )
-            
-            }
+       <Header transparent  style={styles.header}>
+        <StatusBar backgroundColor="transparent" hidden={true} barStyle="light-content"/>
+          <Left style={{flex:1}}>
+            <Button transparent>
+              <HandBugger width={30*rem} height={30*rem}/>
+            </Button>
           </Left>
-          <Body>
-            { Platform.OS!=='android'? <Title style={[styles.title, style]}> {screenName}</Title> : <Title style={[styles.title]}>{screenName}</Title> } 
+          <Body style={{flex:1}}>
+            <Title style={styles.textStyle}>HOME</Title>
           </Body>
-          <Right>
+          <Right style={{flex:1}}>
+            <Button transparent>
+                <Image resizeMode="cover" style={styles.userImage} source={require('../assets/images/image.jpeg')}/>
+            </Button>
           </Right>
-        </Header>
-        </View>
+       </Header>
     );
 }
 const styles = EStyleSheet.create({
     header:{
-        ...Platform.select({
-            ios:{
-                backgroundColor: "#252525" ,
-                 borderBottomWidth: "0rem",
-            },
-            android:{
-                backgroundColor:'#121212',
-                borderBottomWidth: '1rem',
-                borderColor: "#000",
-                borderBottomColor: "#d3d3d3",
-            }
-            
-        }),
+       backgroundColor: "#0C0C0E",
+       paddingTop: '48rem',
+       paddingBottom: '20rem',
+       paddingLeft: '20rem',
+       height:"90rem",
+       borderWidth:'0rem',
+       borderColor:'#0c0c0e'
     },
-    title:{
-        fontSize: '22rem', 
+    userImage:{
+        height: '50rem',
+        width:'50rem',
+        borderRadius: '70rem',
+        borderWidth: '2rem',
+        borderColor: "#FF912C",
+        alignSelf: 'center',
+    },
+    textStyle:{
         color:"#fff",
-        fontFamily:"Itim",
-        textAlign:'center'
-    },
-    backText:{
-        ...Platform.select({
-            ios:{
-                fontSize: '20rem', 
-                color:"#fff",
-                fontFamily:"Itim",
-            },
-            android:{
-              
-            }
-        })
-    },
-    icon:{
-        color:"#fff"
+        alignSelf: 'center',
+        paddingTop:0,
+        justifyContent: 'center',
     }
-   
 })
 export default HeaderComponent;
