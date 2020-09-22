84847c773030d2dcc82205d9a1a0808f41a939d9
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-09-22 19:45:52 +0100

    made change

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ const entireScreenWidth = Dimensions.get('window').width;
 const rem = entireScreenWidth/380
 import changeNavigationBarColor from 'react-native-navigation-bar-color';
 
+
 const theme = {...dark, ...appTheme}
 
 EStyleSheet.build({
