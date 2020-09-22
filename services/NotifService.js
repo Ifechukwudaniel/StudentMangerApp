05052642c6a2982128e273e3a7aa50d891c78779
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-09-22 19:46:43 +0100

    naotification

---
+++ services/NotifService.js
@@ -0,0 +1,54 @@
+import PushNotification from 'react-native-push-notification'
+
+// Must be outside of any component LifeCycle (such as `componentDidMount`).
+PushNotification.configure({
+  onRegister: function (token) {
+    console.log("TOKEN:", token);
+  },
+ 
+  // (required) Called when a remote is received or opened, or local notification is opened
+  onNotification: function (notification) {
+    console.log("NOTIFICATION:", notification);
+    notification.finish(PushNotificationIOS.FetchResult.NoData);
+  },
+ 
+  // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+  onAction: function (notification) {
+    console.log("ACTION:", notification.action);
+    console.log("NOTIFICATION:", notification);
+ 
+    // process the action
+  },
+ 
+  // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+  onRegistrationError: function(err) {
+    console.error(err.message, err);
+  },
+ 
+  // IOS ONLY (optional): default: all - Permissions to register.
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+
+  popInitialNotification: true,
+ 
+  requestPermissions: true,
+});
+export const LocalNotification = () => {
+  PushNotification.localNotification({
+    autoCancel: true,
+    bigText:
+      'This is local notification demo in React Native app. Only shown, when expanded.',
+    subText: 'Local Notification Demo',
+    title: 'Local Notification Title',
+    message: 'Expand me to see more',
+    vibrate: true,
+    vibration: 300,
+    playSound: true,
+    soundName: 'default',
+    color: "red",
+    // actions: '["Yes", "No"]'
+  })
+}
