ac44eda7e07b84466144ae22127f1c7909f8b339
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-10-03 22:46:28 +0100

    made changes

---
+++ components/icons/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import ThreeDS  from '../../assets/svg/fileIcons/3ds.svg'
+import AI  from '../../assets/svg/fileIcons/ai.svg'
+import ASP  from '../../assets/svg/fileIcons/asp.svg'
+import AVI  from '../../assets/svg/fileIcons/avi.svg'
+import BIN  from '../../assets/svg/fileIcons/bin.svg'
+import COM  from '../../assets/svg/fileIcons/com.svg'
+import CSS  from '../../assets/svg/fileIcons/css.svg'
+import CSV  from '../../assets/svg/fileIcons/csv.svg'
+import DBF  from '../../assets/svg/fileIcons/dbf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+import PDF  from '../../assets/svg/fileIcons/pdf.svg'
+
+
+const icons = [
+  {
+    "pdf" :require('../../assets/svg/fileIcons/pdf.svg')
+  }
+]
+
+ 
+const FileIcons  = () => {
+  return (
+    
+  );
+}
+ 
+export default FileIcons
